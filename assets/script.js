7406d29f0b50c537a1266795e11919e7ebb813ca
forbegos <forbegos@me.com>
2021-05-05 08:39:01 -0600

    started JS script for testing Yelp api connection

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+// Truck Locator Application - Javascript
+
+// Variable Declaration
+// 1. Idenfity selector for user food choice (dropdown selection)
+// 2.
+
+// Get truck locator api data
+// 1. declare url variable (add food choice to search query)
+// 2. fetch .then functions
+
+// Location identifier - how do we get user current location??
+
+// Get mapbox api data
+// 1. declare url variable (add truck address from locator api (lat, long? Address?) choice to search query)
+// 2. fetch .then functions
+
+// Get yelp api data
+
+yelpRequestAPI = "https://api.jquery.com/jquery.getjson/ ";
+
+fetch(yelpRequestAPI).then(function (response) {
+  console.log(response.json());
+  return response.json();
+});
+
+// 1. declare url variable (add truck choice to search query)
+// 2. fetch .then functions
