ab73974281ea7ce53d2e8539615283a0f89837ee
forbegos <forbegos@me.com>
2021-05-07 14:09:17 -0600

    search html now showing search results

--- assets/search-results.js
+++ assets/search-results.js
@@ -4,7 +4,31 @@ console.log(searchQuery);
 
 // Get search results from API
 
-var cities = [];
+function displaySearchResults(trucksObject) {
+  console.log(trucksObject.name);
+  var truckCard = document.createElement("div");
+  truckCard.classList.add("card", "bg-light", "text-dark", "mb-3", "p-3");
+
+  var cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
+  truckCard.append(cardBody);
+
+  var cardTitle = document.createElement("h3");
+  cardTitle.textContent = trucksObject.name;
+
+  var truckDescription = document.createElement("p");
+  truckDescription.innerHTML = trucksObject.description + "<br/>";
+
+  var truckAddress = document.createElement("p");
+  truckAddress.innerHTML = "Address: " + trucksObject.last.display + "<br/>";
+
+  var truckURL = document.createElement("p");
+  truckURL.innerHTML = "URL: " + trucksObject.url + "<br/>";
+
+  cardBody.append(cardTitle, truckDescription, truckAddress, truckURL);
+  resultsEl = document.getElementById("results");
+  resultsEl.append(cardBody);
+}
 
 fetch(
   "https://cors-anywhere.herokuapp.com/http://data.streetfoodapp.com/1.1/schedule/" +
@@ -16,24 +40,10 @@ fetch(
   })
   .then(function (data) {
     var arr = Object.keys(data).map((key) => [key, data[key]]);
-    console.log(arr);
     var arr2 = Object.keys(arr[1][1]).map((key) => [key, arr[1][1][key]]);
 
-    // console.log(arr2);
-    // console.log(arr2[0][0]);
-    // console.log(arr2[0][1].description);
-
-    var listEl = document.getElementById("search-results");
-    var nameEl = document.createElement("li");
-    nameEl.innerHTML = arr2[0][0];
-    console.log(nameEl.text);
-    listEl.appendChild(nameEl);
-
-    //   for (var i = 0; i < arr.length; i++) {
-    //         var city = arr[i][1].name;
-    //   var country = arr[i][1].country;
-    //   if (country == "us") {
-    //     cities.push(city);
-    //   }
-    // }
+    console.log(arr2);
+    for (var i = 0; i < arr2.length; i++) {
+      displaySearchResults(arr2[i][1]);
+    }
   });
